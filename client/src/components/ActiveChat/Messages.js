c6cb4378e498a747226e067fa5627a1cb0f161cb
Joshua Nho <joshnho@gmail.com>
2021-07-04 08:16:47 -0400

    moved dispatching updateReadStatus from ActiveChat.js to Messages.js -- it was dispatching for each message being rendered if unread messages existed in the conversation -- it now only dispatches when scrolled to bottom of messages; added package to scroll to bottom of messages on load;

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ScrollableFeed from "react-scrollable-feed";
+import { connect } from "react-redux";
 import { Box, makeStyles } from "@material-ui/core";
+import { updateReadStatus } from "../../store/utils/thunkCreators";
 import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
 
@@ -8,6 +11,8 @@ const useStyles = makeStyles(() => ({
     width: "100%",
     maxHeight: "70vh",
     overflow: "auto",
+  },
+  messageWindow: {
     // Hide scrollbars for chrome
     "&::-webkit-scrollbar": {
       display: "none"
@@ -16,23 +21,46 @@ const useStyles = makeStyles(() => ({
     "-ms-overflow-style": "none",
     "scrollbar-width": "none",
   },
-}))
+}));
 
-const Messages = ({ messages, otherUser, userId }) => {
+const Messages = ({ userId, conversation, conversation: { messages, otherUser }, updateReadStatus }) => {
+  const [atBottom, setAtBottom] = useState(true)
   const classes = useStyles()
-  return (
-    <Box className={`${classes.root}`}>
-      {messages.map((message) => {
-        const time = moment(message.createdAt).format("h:mm");
 
-        return message.senderId === userId ? (
-          <SenderBubble key={message.id} message={message} time={time} />
-        ) : (
-          <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
-        );
-      })}
+  useEffect(() => {
+    if (atBottom === true) {
+      if (conversation) {
+        updateReadStatus(conversation);
+      };
+    };
+  }, [updateReadStatus, conversation, atBottom])
+
+  return (
+    <Box className={classes.root}>
+      <ScrollableFeed
+        className={classes.messageWindow}
+        onScroll={isAtBottom => setAtBottom(isAtBottom)}
+      >
+        {messages.map((message) => {
+          const time = moment(message.createdAt).format("h:mm");
+
+          return message.senderId === userId ? (
+            <SenderBubble key={message.id} message={message} time={time} />
+          ) : (
+            <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
+          );
+        })};
+      </ScrollableFeed>
     </Box>
   );
 };
 
-export default Messages;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    updateReadStatus: (message) => {
+      dispatch(updateReadStatus(message));
+    },
+  };
+};
+
+export default connect(null, mapDispatchToProps)(Messages);
