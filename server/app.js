1659477aaeb342eabbbd890e1167a427e823837e
Joshua Nho <joshnho@gmail.com>
2022-07-25 01:41:41 -0400

    set up cors; set db base url

--- server/app.js
+++ server/app.js
@@ -1,13 +1,22 @@
-const createError = require("http-errors");
-const express = require("express");
-const { join } = require("path");
-const logger = require("morgan");
-const jwt = require("jsonwebtoken");
-const session = require("express-session");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
-const db = require("./db");
-const { User } = require("./db/models");
-const cookieParser = require("cookie-parser");
+const createError = require('http-errors');
+const express = require('express');
+const { join } = require('path');
+const logger = require('morgan');
+const jwt = require('jsonwebtoken');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const db = require('./db');
+const { User } = require('./db/models');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+// cors config
+app.use(
+  cors({
+    origin: 'https://zingy-beignet-2f1684.netlify.app/',
+    methods: ['GET', 'POST', 'DELETE', 'PATCH'],
+  })
+);
+
 // create store for sessions to persist in database
 const sessionStore = new SequelizeStore({ db });
 
@@ -15,14 +24,14 @@ const { json, urlencoded } = express;
 
 const app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(json());
 app.use(urlencoded({ extended: false }));
-app.use(express.static(join(__dirname, "public")));
-app.use(cookieParser())
+app.use(express.static(join(__dirname, 'public')));
+app.use(cookieParser());
 
 app.use(function (req, res, next) {
-  const token = req.cookies.token
+  const token = req.cookies.token;
   if (token) {
     jwt.verify(token, process.env.SESSION_SECRET, (err, decoded) => {
       if (err) {
@@ -41,8 +50,8 @@ app.use(function (req, res, next) {
 });
 
 // require api routes here after I create them
-app.use("/auth", require("./routes/auth"));
-app.use("/api", require("./routes/api"));
+app.use('/auth', require('./routes/auth'));
+app.use('/api', require('./routes/api'));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -54,7 +63,7 @@ app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   console.log(err);
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,24 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
-});
+const db = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://localhost:5432/messenger',
+  {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    logging: false,
+  }
+);
+
+db.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 module.exports = db;
