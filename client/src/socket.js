911c24db531d5a6fe18e981e7fb442cfb62e00c4
Joshua Nho <joshnho@gmail.com>
2022-07-25 02:55:56 -0400

    set socket io connection url

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,34 +1,34 @@
-import io from "socket.io-client";
-import store from "./store";
+import io from 'socket.io-client';
+import store from './store';
 import {
   setNewMessage,
   removeOfflineUser,
   addOnlineUser,
-  readMessages
-} from "./store/conversations";
+  readMessages,
+} from './store/conversations';
 
-const socket = io(window.location.origin, {
+const socket = io('https://zingy-beignet-2f1684.netlify.app/', {
   autoConnect: false,
 });
 
-socket.on("connect", () => {
-  console.log("connected to server");
+socket.on('connect', () => {
+  console.log('connected to server');
 
-  socket.on("add-online-user", (id) => {
+  socket.on('add-online-user', (id) => {
     store.dispatch(addOnlineUser(id));
   });
 
-  socket.on("remove-offline-user", (id) => {
+  socket.on('remove-offline-user', (id) => {
     store.dispatch(removeOfflineUser(id));
   });
-  socket.on("new-message", (data) => {
+  socket.on('new-message', (data) => {
     store.dispatch(setNewMessage(data.message, data.sender));
   });
-  socket.on("read-messages", (data) => {
-    store.dispatch(readMessages(data.conversationId, data.userId))
-  })
-  socket.on("disconnect", () => {
-    console.log("User has disconnected");
+  socket.on('read-messages', (data) => {
+    store.dispatch(readMessages(data.conversationId, data.userId));
+  });
+  socket.on('disconnect', () => {
+    console.log('User has disconnected');
   });
 });
 
